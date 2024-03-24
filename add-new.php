8cac6ed7adee6ca02fc24037c3c78ec05778760b
Julius Jess Estose <estosejuliusjess@gmail.com>
2024-03-24 11:32:21 +0800

    update

--- add-new.php
+++ add-new.php
@@ -12,7 +12,8 @@ if (isset($_POST["submit"])) {
    $result = mysqli_query($conn, $sql);
 
    if ($result) {
-      header("Location: index.php?msg=New record created successfully");
+      header("Location: index.php?msg= New record created Successfully");
+      
    } else {
       echo "Failed: " . mysqli_error($conn);
    }
@@ -86,6 +87,8 @@ if (isset($_POST["submit"])) {
 
    <!-- Bootstrap -->
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+   <!-- SweetAlert
+   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
 
 </body>
 
--- index.php
+++ index.php
@@ -12,6 +12,9 @@ include "db_conn.php";
 
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  
+  <!-- data tables -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
 
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -36,7 +39,7 @@ include "db_conn.php";
     ?>
     <a href="add-new.php" class="btn btn-dark mb-3">Add Me</a>
 
-    <table class="table table-hover text-center">
+    <table id="myTable" class="table table-hover text-center">
       <thead class="table-dark">
         <tr>
           <th scope="col">ID</th>
@@ -73,6 +76,17 @@ include "db_conn.php";
 
   <!-- Bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+
+  <!-- data tables -->
+  <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+  $(document).ready( function () {
+    $('#myTable').DataTable();
+  });
+</script>
 
 </body>
 
